4f33ca7b1c8e1e30cd55c06347def71fc5205880
Israel Santana de Morais <46788030847@>
2024-12-02 09:33:40 -0300

    adicionando banners

--- web_server.py
+++ web_server.py
@@ -2,28 +2,40 @@ import http.server
 import socketserver
 import ssl
 import os
+#com a chave SSL E RSA
+# PORT = 5050
+# DIRECTORY = "." 
+
+# class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+#     def do_GET(self):
+#         if self.path == '/':
+#             self.path = 'index.html'
+        
+#         return super().do_GET()
+
+# context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+# context.load_cert_chain(certfile="../serverpjt.crt", keyfile="../pojeto.key")
+
+# os.chdir(DIRECTORY)
+
+# with socketserver.ThreadingTCPServer(("", PORT), MyHttpRequestHandler) as httpd:
+#     print(f"Servidor iniciado na porta {PORT}. Acesse https://localhost:{PORT}")
+#     httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+#     httpd.serve_forever()
+
 
 PORT = 5050
-DIRECTORY = "."  # O diretório onde o servidor irá procurar arquivos
+DIRECTORY = "." 
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        # Se a requisição for para a raiz, redireciona para /ecommerce/index.html
         if self.path == '/':
-            self.path = 'index.html'
+            self.path = 'home.html'
         
-        # Chama o método original para servir o arquivo
         return super().do_GET()
-
-# Configura o contexto SSL
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain(certfile="../serverpjt.crt", keyfile="../pojeto.key")
-
-# Define a raiz do diretório onde o servidor irá procurar os arquivos
 os.chdir(DIRECTORY)
 
-# Inicia o servidor com SSL e Threading
+# Inicia o servidor sem SSL e com Threading
 with socketserver.ThreadingTCPServer(("", PORT), MyHttpRequestHandler) as httpd:
-    print(f"Servidor iniciado na porta {PORT}. Acesse https://localhost:{PORT}")
-    httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+    print(f"Servidor iniciado na porta {PORT}. Acesse http://localhost:{PORT}")
     httpd.serve_forever()
