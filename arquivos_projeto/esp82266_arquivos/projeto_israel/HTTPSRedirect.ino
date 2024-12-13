39b3ac899c44f12a7b49c2d082bd81f0f7eb3c25
Israel Santana de Morais <46788030847@>
2024-12-13 11:13:40 -0300

    adicionando chave ssl

--- arquivos_projeto/esp82266_arquivos/projeto_israel/HTTPSRedirect.ino
+++
@@ -1,4 +0,0 @@
-// The file exists only to satisfy the requirement of arduino-cli
-// https://github.com/arduino/arduino-cli/issues/765
-// It needs the main compiled file to have the same name as the repo
-
